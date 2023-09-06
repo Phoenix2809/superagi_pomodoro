38e2629694932398d26c732fe85565bcdb908952
Phoenix2809 <133874957+Phoenix2809@users.noreply.github.com>
2023-09-06 18:34:04 +0530

    Update multiply.py
    wrote code

--- multiply.py
+++ multiply.py
@@ -1,2 +1,24 @@
-placeholder for code
-this is some seriously terrible code. it's what it is. 
+def multiply_numbers(num1, num2, num3, num4, num5):
+    result = num1 * num2 * num3 * num4 * num5
+    return result
+
+# Bug 1: Incorrect parameter order
+result = multiply_numbers(2, 3, 4, 5, 6)
+
+# Bug 2: Using undefined variables
+result = multiply_numbers(num1, num2, num3, num4, num5)
+
+# Bug 3: Using string values instead of numbers
+result = multiply_numbers("2", "3", "4", "5", "6")
+
+# Bug 4: Division instead of multiplication
+result = multiply_numbers(10, 2, 5, 2, 1/2)
+
+# Bug 5: Missing return statement
+def multiply_numbers_missing_return(num1, num2, num3, num4, num5):
+    result = num1 * num2 * num3 * num4 * num5
+
+# Bug 6: Incorrect indentation
+    return result
+
+result = multiply_numbers_missing_return(2, 3, 4, 5, 6)
